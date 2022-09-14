e2b89df241750683260b6b920211a9b82f038464
Milton Paredes <mhplur@hotmail.com>
2022-09-14 14:02:08 -0500

    Update

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,11 @@
-namespace SitioWebSura.Models;
+using System;
 
-public class ErrorViewModel
+namespace SitioWebSura.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
